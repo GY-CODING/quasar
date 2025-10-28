02b5c177ee32d483fea47c2ca60f8bf386b563e3
ToxYc <ivi1000vm@gmail.com>
2025-10-28 17:27:52 +0100

    fix: Fix annotation

--- src/main/java/org/gycoding/quasar/annotations/QuasarApplication.java
+++ src/main/java/org/gycoding/quasar/annotations/QuasarApplication.java
@@ -1,18 +1,25 @@
 package org.gycoding.quasar.annotations;
 
+import org.gycoding.quasar.configs.APIFilter;
+import org.gycoding.quasar.configs.CORSConfig;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @EnableFeignClients
 @EnableMongoRepositories
 @ComponentScan(basePackages = {"org.gycoding"})
+@Import({ CORSConfig.class, APIFilter.class })
 public @interface QuasarApplication { }
